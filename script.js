b9c286e5b399b033208593bd8880cb76c20e868c
Mihai <dudehacksypu@gmail.com>
2023-05-14 19:25:58 +0300

    Main

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const path = require('path');
@@ -14,6 +14,9 @@ app.use(express.static(path.join(__dirname, 'templates')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//url
+const url = "http://cetateanul-targujiu.com";
+
 // Load the questions from the q.json file
 const questions = require('./q.json');
 
@@ -21,7 +24,7 @@ const questions = require('./q.json');
 const questionStrings = questions.map(question => question.question);
 
 // Handle POST requests to the /submit route
-app.post('/submit', async (req, res) => {
+app.post('${url}/submit', async (req, res) => {
     const { name, username, userData } = req.body;
 
     // Validate the data received in the POST request
@@ -59,11 +62,11 @@ app.post('/submit', async (req, res) => {
             const headerRow = [
                 { header: 'Nume', key: 'name' },
                 { header: 'Prenume', key: 'username' },
-                { header: 'Varsta', key: 'answer-1' },
-                { header: 'Culoare preferata', key: 'answer-2' },
-                { header: 'Mancare preferata', key: 'answer-3' },
-                { header: 'Locatie', key: 'answer-4' },
-                { header: 'Carte preferata', key: 'answer-5' },
+                { header: 'Credeți că autoritățile locale din Târgu Jiu gestionează adecvat problema deșeurilor?', key: 'answer-1' },
+                { header: 'Sunteți de acord cu măsurile luate de autorități pentru a reduce cantitatea de deșeuri generate de oraș?', key: 'answer-2' },
+                { header: 'Credeți că publicul este suficient de informat cu privire la modul în care trebuie să gestioneze deșeurile?', key: 'answer-3' },
+                { header: 'Sunteți mulțumit/mulțumită cu numărul de coșuri de gunoi?', key: 'answer-4' },
+                { header: 'Credeți că Târgu Jiu ar trebui să adopte o politică de reducere a cantității de ambalaje din plastic utilizate în oraș?', key: 'answer-5' },
                 ...questions.map(question => ({ header: `Q${question.number}`, key: `Q${question.number}` })),
             ];
             utils.book_append_sheet(workbook, utils.json_to_sheet([], { header: headerRow.map(col => col.header) }), 'Sheet1');
@@ -77,11 +80,11 @@ app.post('/submit', async (req, res) => {
     const newSubmission = {
         'Nume': name,
         'Prenume': username,
-        'Varsta': userDataValues[0],
-        'Culoare preferata': userDataValues[1],
-        'Mancare preferata': userDataValues[2],
-        'Locatie': userDataValues[3],
-        'Carte preferata': userDataValues[4],
+        'Credeți că autoritățile locale din Târgu Jiu gestionează adecvat problema deșeurilor?': userDataValues[0],
+        'Sunteți de acord cu măsurile luate de autorități pentru a reduce cantitatea de deșeuri generate de oraș?': userDataValues[1],
+        'Credeți că publicul este suficient de informat cu privire la modul în care trebuie să gestioneze deșeurile?': userDataValues[2],
+        'Sunteți mulțumit/mulțumită cu numărul de coșuri de gunoi?': userDataValues[3],
+        'Credeți că Târgu Jiu ar trebui să adopte o politică de reducere a cantității de ambalaje din plastic utilizate în oraș?': userDataValues[4],
         ...questions.reduce((acc, question, index) => {
             acc[`Q${question.number}`] = userDataValues[index + 5];
             return acc;
@@ -101,11 +104,11 @@ app.post('/submit', async (req, res) => {
 });
 
 // Serve the questions on the /questions route
-app.get('/questions', (req, res) => {
+app.get('${url}/questions', (req, res) => {
     res.send(questions);
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening at http://cetateanul-targujiu.com:${port}`);
+    console.log(`Server listening at ${url}`);
 });
